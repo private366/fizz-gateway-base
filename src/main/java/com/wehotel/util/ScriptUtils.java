6b0d6eaea44d3810a179f6a15d376f13f476029b
lancer.hong_洪巧维 <lancer.hong@wehotelglobal.com>
2020-03-13 19:11:20 +0800

    reuse script engine.

--- src/main/java/com/wehotel/util/ScriptUtils.java
+++ src/main/java/com/wehotel/util/ScriptUtils.java
@@ -10,19 +10,38 @@ import java.util.Map;
  * @author Lancer Hong
  */
 
-public interface ScriptUtils {
+public abstract class ScriptUtils {
 
-    static final String JAVA_SCRIPT = "javascript";
+    static final String JAVA_SCRIPT = "javascript"; // 不打算支持
 
     static final String GROOVY = "groovy";
+
+    private static ScriptEngine groovyEngine;
+
+    private static ScriptEngine javascriptEngine;
+
+    static {
+        ScriptEngineManager engineManger = new ScriptEngineManager();
+        groovyEngine = engineManger.getEngineByName(GROOVY);
+        javascriptEngine = engineManger.getEngineByName(JAVA_SCRIPT);
+    }
+
+    public static ScriptEngine getScriptEngine(String type) throws ScriptException {
+        if (GROOVY.equals(type)) {
+            return groovyEngine;
+        } else if (JAVA_SCRIPT.equals(type)) {
+            return javascriptEngine;
+        } else {
+            throw new ScriptException("unknown script engine type: " + type);
+        }
+    }
 
     static Object execute(Script script) throws ScriptException {
         return execute(script, null);
     }
 
     static Object execute(Script script, Map<String, Object> context) throws ScriptException {
-        ScriptEngineManager engineManger = new ScriptEngineManager();
-        ScriptEngine engine = engineManger.getEngineByName(script.getType());
+        ScriptEngine engine = getScriptEngine(script.getType());
         String src = script.getSource();
         if (context == null) {
             return engine.eval(src);
